731d263bf3557aa65352517744f4f5e4ae56add9
Adam <61752657+ToxicSkill@users.noreply.github.com>
2023-09-03 10:22:51 +0200

    Add - Goog looking labels control

--- AI/Models/YoloLabel.cs
+++ AI/Models/YoloLabel.cs
@@ -4,11 +4,25 @@ namespace YoLoTool.AI.Models
 {
     public class YoloLabel
     {
+        private string _name = "";
+
         public int Id { get; set; }
 
-        public string? Name { get; set; }
+        public string? Name 
+        { 
+            get => _name;
+            set
+            {
+                if (_name == value) return;
+                if (string.IsNullOrEmpty(value)) return;
+                _name = value;
+                Width = _name!.Length * 10;
+            }
+        }
 
         public YoloLabelKind Kind { get; set; }
+        
+        public int Width { get; set; }
 
         public Color Color { get; set; }
 
